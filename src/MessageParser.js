712a33e58455ffd13481cd6484ecffa412fa09bf
priyavkaneria <priyavkaneria@gmail.com>
2021-02-16 12:37:31 +0530

    Final commit backend working

--- src/MessageParser.js
+++ src/MessageParser.js
@@ -18,7 +18,7 @@ class MessageParser {
             // Fetch data using temp and message
             var formData = new FormData();
             formData.append("search",message)
-            fetch('https://https://dev-assist-api.herokuapp.com/api/inputValue',
+            fetch('https://dev-assist-api.herokuapp.com/api/inputValue',
                 {method: 'POST', mode: 'cors', body: formData})
             .then(res => res.json())
             .then(data => {
